9f22f54e931a12f64c5c943cb9541e0be3fe8a70
Anton Ivanov <topixare@gmail.com>
2021-04-11 22:41:04 +0300

    feat: implement the function "deleteDigit" ("05-delete-digit-tests.js" task)

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,16 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const numbersArray = n
+    .toString()
+    .split('')
+    .map((elem) => Number(elem));
+  const min = Math.min.apply(null, numbersArray);
+  const minIndex = numbersArray.indexOf(min);
+  numbersArray.splice(minIndex, 1);
+  const numbersString = numbersArray.join('');
+  return Number(numbersString);
 }
 
 module.exports = deleteDigit;
