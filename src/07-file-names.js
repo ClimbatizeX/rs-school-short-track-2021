cf228200e595a5791d2ae62a2b7380b5bbcd4233
Anton Ivanov <topixare@gmail.com>
2021-04-11 23:58:47 +0300

    feat: implement the function "renameFiles" ("07-file-names-tests.js" task)

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,19 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const copyNames = names;
+  for (let i = copyNames.length - 1; i >= 0; i--) {
+    let count = 0;
+    for (let j = 0; j < i; j++) {
+      if (copyNames[i] === copyNames[j]) {
+        if (count > 0) copyNames[j] = `${copyNames[i]}(${count})`;
+        count++;
+      }
+    }
+    if (count > 0) copyNames[i] = `${copyNames[i]}(${count})`;
+  }
+  return copyNames;
 }
 
 module.exports = renameFiles;
