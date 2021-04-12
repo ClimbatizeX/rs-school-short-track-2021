bcc3feca935527ab0434d6954046258a7452ea2d
Anton Ivanov <topixare@gmail.com>
2021-04-12 14:52:34 +0300

    feat: implement the function "encodeLine" ("11-encode-line.js" task)

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str === '') return str;
+  const charArray = str.split('');
+  let actualChar = charArray[0];
+  let count = 0;
+  const resultArray = [];
+  charArray.forEach((char) => {
+    if (char === actualChar) {
+      count++;
+    } else {
+      resultArray.push(`${(count === 1) ? '' : count}${actualChar}`);
+      actualChar = char;
+      count = 1;
+    }
+  });
+  resultArray.push(`${(count === 1) ? '' : count}${actualChar}`);
+  return resultArray.join('');
 }
 
 module.exports = encodeLine;
